2d88453c96c70199938cb54bf62f57ead6994108
diksha-139 <64410938+diksha-139@users.noreply.github.com>
2021-12-18 16:46:02 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -29,13 +29,14 @@ class Form {
 
   //BP
   handleMousePressed() {
-    this.playButton.mousePressed(() => {
+    this.playButton.mousePressed( () => {
       this.input.hide();
       this.playButton.hide();
-      var message = `
-      Hello ${this.input.value()}
-      </br>wait for another player to join...`;
+      var message = "  Hello  "+this.input.value()+"  wait for another player to join..."
+     
       this.greeting.html(message);
+   
+      
       playerCount += 1;
       player.name = this.input.value();
       player.index = playerCount;
